37782406103d198566ac87b951adaa0a950205d2
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-03-13 02:26:47 +0700

    fix: add be cors and metadata routes

--- backend/src/main/java/com/tkpm/sms/controller/MetadataController.java
+++ backend/src/main/java/com/tkpm/sms/controller/MetadataController.java
@@ -2,20 +2,30 @@ package com.tkpm.sms.controller;
 
 import com.tkpm.sms.enums.EnumUtils;
 import com.tkpm.sms.enums.Faculty;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.tkpm.sms.enums.Gender;
+import com.tkpm.sms.enums.Status;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/metadata")
 public class MetadataController {
     @GetMapping("/faculty")
     public List<String> getAllFaculty() {
         return Arrays.stream(EnumUtils.getNames(Faculty.class)).toList();
+    }
+
+    @GetMapping("/student-status")
+    public List<String> getAllStudentStatus() {
+        return Arrays.stream(EnumUtils.getNames(Status.class)).toList();
+    }
+
+    @GetMapping("/gender")
+    public List<String> getAllGender() {
+        return Arrays.stream(EnumUtils.getNames(Gender.class)).toList();
     }
 }
--- backend/src/main/java/com/tkpm/sms/controller/StudentController.java
+++ backend/src/main/java/com/tkpm/sms/controller/StudentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
 public class StudentController {
