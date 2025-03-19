4457c6c37f055eb91d21e9ee6f9e14323b8a3a13
ltt204 <22120371@student.hcmus.edu.vn>
2025-03-19 13:31:58 +0700

    feat: create identity mapper

---
+++ backend/src/main/java/com/tkpm/sms/mapper/IdentityMapper.java
@@ -0,0 +1,22 @@
+package com.tkpm.sms.mapper;
+
+import com.tkpm.sms.dto.request.IdentityCreateRequestDto;
+import com.tkpm.sms.dto.request.IdentityUpdateRequestDto;
+import com.tkpm.sms.dto.response.identity.IdentityDto;
+import com.tkpm.sms.entity.Identity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface IdentityMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "student", ignore = true)
+    Identity createIdentity(IdentityCreateRequestDto identityCreateRequestDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "student", ignore = true)
+    void updateIdentity(@MappingTarget Identity identity, IdentityUpdateRequestDto identityUpdateRequestDto);
+
+    IdentityDto toIdentityDto(Identity identity);
+}
