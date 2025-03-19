b1b2fefff18ebe3bff2f8e86d61be2294753b490
ltt204 <22120371@student.hcmus.edu.vn>
2025-03-19 13:34:07 +0700

    feat: enhance error handling with new identity-related error codes

--- backend/src/main/java/com/tkpm/sms/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/tkpm/sms/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,21 @@
 package com.tkpm.sms.exceptions;
 
 import com.tkpm.sms.dto.response.common.ApplicationResponseDto;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final String VALUES_ATTRIBUTE = "values";
+
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ApplicationResponseDto<Object>> handleApplicationException(ApplicationException exception) {
         log.error("Application error", exception);
@@ -33,10 +39,29 @@ public class GlobalExceptionHandler {
         log.error("Validation error", exception);
 
         var enumKey = exception.getFieldError().getDefaultMessage();
-        var error = ErrorCode.valueOf(enumKey);
+        var error = ErrorCode.INVALID_ERROR_KEY;
+        log.info("Enum key: {}", enumKey);
+        try {
+            error = ErrorCode.valueOf(enumKey);
 
+            var constrainViolation = exception.getBindingResult().getAllErrors().get(0).unwrap(ConstraintViolation.class);
+            var attributes = constrainViolation.getConstraintDescriptor().getAttributes();
+                log.info("Attributes: {}", attributes);
+            if (attributes.containsKey(VALUES_ATTRIBUTE)) {
+                error.setMessage(mapEnumAttribute(error.getMessage(), attributes));
+            }
+
+        } catch (Exception e) {
+            // Ignore
+        }
         var response = ApplicationResponseDto.failure(error);
         return ResponseEntity.badRequest().body(response);
+    }
+
+    private String mapEnumAttribute(String message, Map<String, Object> params) {
+        String[] values = (String[]) params.get(VALUES_ATTRIBUTE);
+
+        return message.replace("{" + "values" + "}", Arrays.toString(values));
     }
 }
 
