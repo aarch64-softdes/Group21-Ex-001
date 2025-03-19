2bf9bc73ca9e5e6c5ed599f14267493d2ca3a841
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2025-03-19 13:41:36 +0700

    add: create Faculty entity and implement CRUD for Faculty

---
+++ backend/src/main/java/com/tkpm/sms/service/FacultyService.java
@@ -0,0 +1,61 @@
+package com.tkpm.sms.service;
+
+import com.tkpm.sms.dto.request.FacultyRequestDto;
+import com.tkpm.sms.entity.Faculty;
+import com.tkpm.sms.exceptions.ApplicationException;
+import com.tkpm.sms.exceptions.ErrorCode;
+import com.tkpm.sms.repository.FacultyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class FacultyService {
+    FacultyRepository facultyRepository;
+    
+    public List<Faculty> getAllFaculties() {
+        return facultyRepository.findAll();
+    }
+
+    public Faculty getFacultyById(Integer id) {
+        return facultyRepository.findById(id).orElseThrow(
+                () -> new ApplicationException(ErrorCode.NOT_FOUND.withMessage(String.format("Faculty with id %s not found", id))));
+    }
+
+    public Faculty getFacultyByName(String name) {
+        return facultyRepository.findFacultyByName(name.toLowerCase()).orElseThrow(
+                () -> new ApplicationException(ErrorCode.NOT_FOUND.withMessage(String.format("Faculty with name %s not found", name))));
+    }
+
+    public Faculty createFaculty(FacultyRequestDto faculty) {
+        if(facultyRepository.existsFacultyByName(faculty.getName())) {
+            throw new ApplicationException(ErrorCode.CONFLICT.withMessage(String.format("Faculty with name %s already existed", faculty.getName())));
+        }
+
+        var newFaculty = Faculty.builder().name(faculty.getName().toLowerCase()).build();
+        return facultyRepository.save(newFaculty);
+    }
+
+    public Faculty updateFaculty(Integer id, FacultyRequestDto faculty) {
+        if(facultyRepository.existsFacultyByName(faculty.getName().toLowerCase())) {
+            throw new ApplicationException(ErrorCode.CONFLICT.withMessage(String.format("Faculty with name %s already existed", faculty.getName())));
+        }
+
+        Faculty facultyToUpdate = facultyRepository.findById(id).orElseThrow(
+                () -> new ApplicationException(ErrorCode.NOT_FOUND.withMessage(String.format("Faculty with id %s not found", id))));
+        facultyToUpdate.setName(faculty.getName());
+        return facultyRepository.save(facultyToUpdate);
+    }
+
+    public void deleteFaculty(Integer id) {
+        var faculty = facultyRepository.findById(id).orElseThrow(
+                () -> new ApplicationException(ErrorCode.NOT_FOUND.withMessage(String.format("Faculty with id %s not found", id))));
+        faculty.setDeletedAt(LocalDate.now());
+        facultyRepository.save(faculty);
+    }
+}
