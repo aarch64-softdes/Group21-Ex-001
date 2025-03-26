5b5691694bcee3367295363699819c39b936c77d
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2025-03-26 20:01:12 +0700

    chore: Create a method for updating phone number setting

--- backend/src/main/java/com/tkpm/sms/controller/SettingController.java
+++ backend/src/main/java/com/tkpm/sms/controller/SettingController.java
@@ -64,4 +64,13 @@ public class SettingController {
         var response = ApplicationResponseDto.success(settingMapper.toSettingDto(updatedSetting));
         return ResponseEntity.ok(response);
     }
+
+    @PutMapping("/phone-number")
+    public ResponseEntity<ApplicationResponseDto<SettingDto>> updatePhoneSetting(
+            @RequestBody SettingRequestDto settingRequestDto
+    ){
+        var updatedSetting = settingService.updatePhoneSetting(settingRequestDto);
+        var response = ApplicationResponseDto.success(settingMapper.toSettingDto(updatedSetting));
+        return ResponseEntity.ok(response);
+    }
 }
