e9decb916191ac3e0761f93daf1d1ede8b4236d1
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2025-03-26 19:59:52 +0700

    change: change pattern of phone number

--- backend/src/main/java/com/tkpm/sms/dto/request/phone/PhoneRequestDto.java
+++ backend/src/main/java/com/tkpm/sms/dto/request/phone/PhoneRequestDto.java
@@ -14,6 +14,6 @@ public class PhoneRequestDto {
     String countryCode;
 
     @RequiredConstraint(field = "Phone number")
-    @Pattern(regexp = "^\\+?\\d+$", message = "INVALID_PHONE_NUMBER")
+    @Pattern(regexp = "^(\\+\\d{1,3})?0?\\d{6,14}$", message = "INVALID_PHONE_NUMBER")
     String phoneNumber;
 }
