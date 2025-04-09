5a6ef3445db8ec27a9bff9b7b1100e608b25559a
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-04-09 22:37:19 +0700

    fix: gap between parts in settings page

--- frontend/src/features/settings/settingPage.tsx
+++ frontend/src/features/settings/settingPage.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const SettingPage: React.FC = () => {
   return (
-    <div className='min-h-1/4 gap-4 p-4'>
+    <div className='grid min-h-1/4 gap-4 p-4'>
       <EmailDomainSettings />
       <PhoneSettings />
     </div>
