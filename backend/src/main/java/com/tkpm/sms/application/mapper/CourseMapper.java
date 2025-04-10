736c6711e6fb8a41b0a9b5e798c3e4367bd8256d
ltt204 <22120371@student.hcmus.edu.vn>
2025-04-10 22:32:26 +0700

    feat: add CourseMapper and CoursePersistenceMapper

---
+++ backend/src/main/java/com/tkpm/sms/application/mapper/CourseMapper.java
@@ -0,0 +1,29 @@
+package com.tkpm.sms.application.mapper;
+
+import com.tkpm.sms.application.dto.request.course.CourseCreateRequestDto;
+import com.tkpm.sms.application.dto.request.course.CourseUpdateRequestDto;
+import com.tkpm.sms.application.dto.response.course.CourseDto;
+import com.tkpm.sms.application.dto.response.course.CourseMinimalDto;
+import com.tkpm.sms.domain.model.Course;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface CourseMapper {
+    @Mapping(target = "schedule", expression = "java(createRequestDto.getSchedule().toString())")
+    Course toDomain(CourseCreateRequestDto createRequestDto);
+
+    @Mapping(target = "schedule", expression = "java(updateRequestDto.getSchedule().toString())")
+    void toDomain(@MappingTarget Course course, CourseUpdateRequestDto updateRequestDto);
+
+    @Mapping(target = "subject", source = "subject.name")
+    @Mapping(target = "code", source = "subject.code")
+    @Mapping(target = "program", source = "program.name")
+    @Mapping(target = "credits", source = "subject.credits")
+    CourseDto toDto(Course entity);
+
+    @Mapping(target = "subject", source = "subject.name")
+    @Mapping(target = "code", source = "subject.code")
+    CourseMinimalDto toMinimalDto(Course entity);
+}
