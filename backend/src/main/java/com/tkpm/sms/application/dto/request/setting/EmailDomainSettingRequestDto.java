fc6f5fdeb696c970c1af48e0cf2287dc0ac089b8
ltt204 <22120371@student.hcmus.edu.vn>
2025-04-10 10:08:02 +0700

    hotfix: replace NotEmpty validation with RequiredConstraint annotation in DTOs; add http status mapping when return validation exception

--- backend/src/main/java/com/tkpm/sms/application/dto/request/setting/EmailDomainSettingRequestDto.java
+++ backend/src/main/java/com/tkpm/sms/application/dto/request/setting/EmailDomainSettingRequestDto.java
@@ -1,10 +1,12 @@
 package com.tkpm.sms.application.dto.request.setting;
 
+import com.tkpm.sms.application.annotation.RequiredConstraint;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class EmailDomainSettingRequestDto {
+    @RequiredConstraint(field = "Email domain")
     String domain;
 }
