ff4617b98c03bf2a191a2190901068f065243315
ltt204 <22120371@student.hcmus.edu.vn>
2025-04-10 22:31:41 +0700

    feat: add  DTOs and schedule constraint for course creation and updates

---
+++ backend/src/main/java/com/tkpm/sms/application/dto/request/course/CourseUpdateRequestDto.java
@@ -0,0 +1,36 @@
+package com.tkpm.sms.application.dto.request.course;
+
+import com.tkpm.sms.application.annotation.CourseScheduleConstraint;
+import com.tkpm.sms.application.annotation.RequiredConstraint;
+import jakarta.validation.Valid;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CourseUpdateRequestDto {
+    @RequiredConstraint(field = "Course year")
+    int year;
+    @RequiredConstraint(field = "Course semester")
+    int semester;
+    @RequiredConstraint(field = "Lecturer")
+    String lecturer;
+    @RequiredConstraint(field = "Max student")
+    int maxStudent;
+    @RequiredConstraint(field = "Course room")
+    String room;
+
+    @Valid
+    @CourseScheduleConstraint
+    @RequiredConstraint(field = "Course schedule")
+    CourseScheduleDto schedule;
+
+    @RequiredConstraint(field = "Course start date")
+    LocalDate startDate;
+}
