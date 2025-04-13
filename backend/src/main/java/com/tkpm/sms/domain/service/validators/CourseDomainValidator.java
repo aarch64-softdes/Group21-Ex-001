c3518866c2cf05789aa3a61d7b2020e62d32d1cb
ltt204 <22120371@student.hcmus.edu.vn>
2025-04-13 18:26:40 +0700

    refactor: create bean for `CourseDomainValidator` instead of use `@Component`

--- backend/src/main/java/com/tkpm/sms/domain/service/validators/CourseDomainValidator.java
+++ backend/src/main/java/com/tkpm/sms/domain/service/validators/CourseDomainValidator.java
@@ -1,13 +1,10 @@
 package com.tkpm.sms.domain.service.validators;
 
 import com.tkpm.sms.domain.exception.DuplicateResourceException;
-import com.tkpm.sms.domain.model.Course;
 import com.tkpm.sms.domain.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
-@Component
 public class CourseDomainValidator {
     private final CourseRepository courseRepository;
 
@@ -19,7 +16,7 @@ public class CourseDomainValidator {
         }
     }
 
-    public void validateRoomAndCourseScheduleForUpdate(String id, String room, String schedule) {
+    public void validateRoomAndCourseScheduleForUpdate(Integer id, String room, String schedule) {
         if (courseRepository.existsByIdNotAndRoomAndCourseSchedule(id, room, schedule)) {
             throw new DuplicateResourceException(
                     String.format("Another course with room %s and schedule %s already exists", room, schedule)
