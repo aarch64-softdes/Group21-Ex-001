7b9784aedeba945e516f75bffaf19fc7d0d4f350
scider <22120134@student.hcmus.edu.vn>
2025-05-08 21:49:38 +0700

    fix: saving translation and adding missing class in WebMvcConfig

--- backend/src/main/java/com/tkpm/sms/infrastructure/config/WebMvcConfig.java
+++ backend/src/main/java/com/tkpm/sms/infrastructure/config/WebMvcConfig.java
@@ -2,9 +2,20 @@ package com.tkpm.sms.infrastructure.config;
 
 import com.tkpm.sms.infrastructure.interceptor.ContentLanguageInterceptor;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 @AllArgsConstructor
