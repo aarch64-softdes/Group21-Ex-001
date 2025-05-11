77d9497aa4faa71914f59349c522867b8b3a09dd
ltt204 <22120371@student.hcmus.edu.vn>
2025-05-11 12:52:16 +0700

    refactor: format query annotation for better readability in EnrollmentJpaRepository

--- backend/src/main/java/com/tkpm/sms/infrastructure/persistence/jpa/EnrollmentJpaRepository.java
+++ backend/src/main/java/com/tkpm/sms/infrastructure/persistence/jpa/EnrollmentJpaRepository.java
@@ -8,11 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Collection;
 
 public interface EnrollmentJpaRepository extends JpaRepository<EnrollmentEntity, Integer> {
-    @Query("SELECT DISTINCT e FROM EnrollmentEntity e " +
-            "LEFT JOIN FETCH e.student st " +
-            "LEFT JOIN FETCH e.course c " +
-            "LEFT JOIN FETCH e.score sc " +
-            "WHERE st.id = :studentId")
+    @Query("""
+            SELECT DISTINCT e FROM EnrollmentEntity e 
+            LEFT JOIN FETCH e.student st 
+            LEFT JOIN FETCH e.course c LEFT JOIN FETCH e.score sc 
+            WHERE st.id = :studentId
+            """)
     Page<EnrollmentEntity> findAllEnrollmentOfStudent(String studentId, Pageable pageable);
 
     EnrollmentEntity findByStudentIdAndCourseId(String studentId, Integer courseId);
